fd7da07a8e40165655c8c6cd676844396051a791
Paul Carrillo <paulcarrillo05@gmail.com>
2016-11-04 17:37:39 -0600

    RPS is styled. Changing colors for result

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-  var $btn = $("button");
+  var $btn = $("button"),
+    $resultBox = $('.result-item'),
+    $user = $('#user'),
+    $computer = $('#computer'),
+    win = 'win',
+    tie = 'tie',
+    lose = 'lose',
+    total = 'win lose tie';
+
+  function winner() {
+    $user.addClass(win);
+    $computer.addClass(lose);
+  }
+
+  function tied() {
+    $resultBox.addClass(tie);
+  }
+
+  function loser() {
+    $user.addClass(lose);
+    $computer.addClass(win);
+  }
 
   // Function for the computer choice, returns value on function call
   function compChoice() {
@@ -21,25 +42,40 @@ $(document).ready(function() {
     console.log(user + '==' + comp);
 
     if (user === comp) {
-      console.log('tie');
-      return false;
-    } else if (user === "Rock") {
-      if (comp === "Scissors") {
-        console.log("Rock beats Scissors");
+      tied();
+      if (comp === "Rock") {
+        $resultBox.find('img').attr('src', 'images/Rock.png');
+      } else if (comp === "Paper") {
+        $resultBox.find('img').attr('src', 'images/Paper.png');
       } else {
-        console.log("Rock loses Paper");
+        $resultBox.find('img').attr('src', 'images/Scissors.png');
+      }
+    } else if (user === "Rock") {
+      $user.find('img').attr('src', 'images/Rock.png');
+      if (comp === "Scissors") {
+        $computer.find('img').attr('src', 'images/Scissors.png');
+        winner();
+      } else if (comp === "Paper") {
+        $computer.find('img').attr('src', 'images/Paper.png');
+        loser();
       }
     } else if (user === "Paper") {
+      $user.find('img').attr('src', 'images/Paper.png');
       if (comp === "Rock") {
-        console.log("Paper beats Rock");
+        $computer.find('img').attr('src', 'images/Rock.png');
+        winner();
       } else {
-        console.log("Paper loses Scissors");
+        $computer.find('img').attr('src', 'images/Scissors.png');
+        loser();
       }
     } else if (user === "Scissors") {
+      $user.find('img').attr('src', 'images/Scissors.png');
       if (comp === "Paper") {
-        console.log("Scissors beats Paper");
+        $computer.find('img').attr('src', 'images/Paper.png');
+        winner();
       } else {
-        console.log("Scissors loses Rock");
+        $computer.find('img').attr('src', 'images/Rock.png');
+        loser();
       }
     }
   }
@@ -47,7 +83,7 @@ $(document).ready(function() {
   // User btn click, and run result function passing user's choice and computers.
   $btn.click(function() {
     var userChoice = $(this).text();
-
+    $resultBox.removeClass(total);
     result(userChoice, compChoice());
   });
 });
