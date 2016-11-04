981992989243962d409eab65d8ae611070da18c5
Alexander Orr <alexanderorr@Alexanders-MacBook-Pro.local>
2016-11-04 14:03:38 -0600

    some structure, compass/scss

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
-  var active = 'is-active';
 
-  function callName() {
-    var $btn = $('.call-btn'),
-        string = 'Paul + Alex = learning something';
-
-    $btn.click(function() {
-      $(this).addClass(active);
-      console.log(string);
-    });
-  }
-
-
-
-  callName();
 });
