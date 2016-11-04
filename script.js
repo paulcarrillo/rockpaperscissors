eebebb080a2c63454dc4daac61c988fe1fcbb22c
Paul Carrillo <paulcarrillo05@gmail.com>
2016-11-04 15:50:49 -0600

    Gave RPS function

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
+  var $btn = $("button");
 
+  function compChoice() {
+    var computerChoice = Math.random();
+
+    if (computerChoice < 0.34) {
+      computerChoice = "Rock";
+    } else if (computerChoice <= 0.67) {
+      computerChoice = "Paper";
+    } else {
+      computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+  }
+
+  function result(user, comp) {
+    console.log(user + '==' + comp);
+
+    if (user === comp) {
+      console.log('tie');
+      return false;
+    } else if (user === "Rock") {
+      if (comp === "Scissors") {
+        console.log("Rock beats Scissors");
+      } else {
+        console.log("Rock loses Paper");
+      }
+    } else if (user === "Paper") {
+      if (comp === "Rock") {
+        console.log("Paper beats Rock");
+      } else {
+        console.log("Paper loses Scissors");
+      }
+    } else if (user === "Scissors") {
+      if (comp === "Paper") {
+        console.log("Scissors beats Paper");
+      } else {
+        console.log("Scissors loses Rock");
+      }
+    }
+  }
+
+  $btn.click(function() {
+    var userChoice = $(this).text();
+
+    result(userChoice, compChoice());
+  });
 });
